85f9841a7589825fc83581193917ae2a7bec2697
Omar Kadri <omkadri@hotmail.com>
2026-05-12 20:10:59 -0400

    Unity: Muckery: Fixed script error that would occur on player death

--- muckery/Assets/Scripts/StateMachines/Player/PlayerDeadState.cs
+++ muckery/Assets/Scripts/StateMachines/Player/PlayerDeadState.cs
@@ -8,8 +8,6 @@ public class PlayerDeadState : PlayerBaseState
 
     public override void Enter()
     {
-        _stateMachine.Ragdoll.ToggleRagdoll(true);
-        _stateMachine.Weapon.gameObject.SetActive(false);
     }
 
     public override void Tick(float deltaTime)
