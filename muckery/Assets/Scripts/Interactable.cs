9396722fe8ec8017e2bb7b2f782067192a970142
Omar Kadri <omkadri@hotmail.com>
2026-05-15 19:38:10 -0400

    Unity: Muckery: Created Intractable Object with enumeration that can set visibility accordingly + created intractable prompt and imported TMP essentials

---
+++ muckery/Assets/Scripts/Interactable.cs
@@ -0,0 +1,66 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public enum InteractableType
+    {
+        Knife,
+        PropaneTank,
+        BananaPeel
+    }
+
+    [SerializeField] InteractableType _interactableType;
+
+    [Header( "Visuals" )]
+    [SerializeField] GameObject _knife;
+    [SerializeField] GameObject _propaneTank;
+    [SerializeField] GameObject _bananaPeel;
+
+    [Header( "UI" )]
+    [SerializeField] GameObject _interactButton;
+
+    void Awake()
+    {
+        UpdateVisuals();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if ( _interactButton != null ) _interactButton.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if ( _interactButton != null ) _interactButton.SetActive(false);
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        EditorApplication.delayCall += DelayedValidate;
+    }
+
+    void DelayedValidate()
+    {
+        if ( this == null )
+            return;
+
+        UpdateVisuals();
+    }
+#endif
+
+    void UpdateVisuals()
+    {
+        if ( _interactButton != null ) _interactButton.SetActive(false);
+
+        if ( _knife != null ) _knife.SetActive( _interactableType == InteractableType.Knife );
+
+        if ( _propaneTank != null ) _propaneTank.SetActive( _interactableType == InteractableType.PropaneTank );
+
+        if ( _bananaPeel != null ) _bananaPeel.SetActive( _interactableType == InteractableType.BananaPeel );
+    }
+}
